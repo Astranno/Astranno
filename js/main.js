46ddde3f094c3f4ee344d77ccd417799c024aa0a
Christopher Rempe <43194904+MrRicearonie@users.noreply.github.com>
2022-10-14 00:05:57 -0700

    Updated color

--- js/main.js
+++ js/main.js
@@ -18,10 +18,12 @@ $(document).ready(function () {
         $(window).scroll(function () {
             if ($(window).scrollTop() >= button) {
                 $("#img-logo-start").css("display","none");
+                $("#name-start").css("display","none");
                 $("body").css("background-color", "#ffffff")
             } else {
                 $("#img-logo-start").css("display","inline");
-                $("body").css("background-color", "gray")
+                $("#name-start").css("display","inline");
+                $("body").css("background-color", "#5bb9e9")
             }
         });
     });
