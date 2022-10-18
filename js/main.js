0fccfba0666f7cca24fafafb3416a7035bbc66c0
Christopher Rempe <43194904+MrRicearonie@users.noreply.github.com>
2022-10-18 16:48:44 -0700

    Working on animation position

--- js/main.js
+++ js/main.js
@@ -1,31 +1,56 @@
 $(document).ready(function () {
+    var screenHeight = $(window).height();
+    console.log(screenHeight)
     var controller = new ScrollMagic.Controller();
 
     var tweenStart = new TweenMax.to('#img-logo-start', 4, {
         scale: 35.00,
-        ease: Sine.easeIn
+        ease: Power2.easeIn
     })
 
-    var sceneStart = new ScrollMagic.Scene({duration: 1000, offset: 0})
+    var sceneStart = new ScrollMagic.Scene({duration: (screenHeight/2), offset: 0})
                         .setTween(tweenStart)
-						.setPin("#starting-page");
-                        // .addIndicators()
+						// .setPin("#starting-page")
+                        .addIndicators();
+
+    var tweenAboutStart = new TimelineMax()
+                        .fromTo("#our-thing span", 1, {opacity: "100%"}, {opacity: "0%", ease: Linear.easeNone})
+
+    var sceneAboutStart = new ScrollMagic.Scene({triggerElement:'#about-us', duration: 100, offset: 0})
+                        .setTween(tweenAboutStart)
+						.setPin("#about-us")
+                        .addIndicators();
+
+    var tweenAboutMove = new TimelineMax()
+                        .fromTo("#our-thing", 1, {x: "0%"}, {x:"-50%"})
+
+    var sceneAboutMove = new ScrollMagic.Scene({triggerElement:'#about-us', duration: 100, offset: 100})
+                        .setTween(tweenAboutMove)
+						.setPin("#about-us")
+                        .addIndicators();
 
     controller.addScene(sceneStart)
+    controller.addScene(sceneAboutStart)
+    controller.addScene(sceneAboutMove)
 
-    var button = $(window).height(); //get offset of second div
-    console.log(button)
     $(function () {
         $(window).scroll(function () {
-            if ($(window).scrollTop() >= button) {
+            if ($(window).scrollTop() >= (screenHeight/2)) {
                 $("#img-logo-start").css("display","none");
                 $("#name-start").css("display","none");
-                $("body").css("background-color", "#ffffff")
+                $("body").css("background-color", "#ffffff");
+                $("#starting-page").removeClass(".fullscreen-container")
             } else {
                 $("#img-logo-start").css("display","inline");
                 $("#name-start").css("display","inline");
                 $("body").css("background-color", "#5bb9e9")
             }
+
+            // if ($(window).scrollTop() >= (screenHight + 1000)) {
+            //     $("#our-thing span").css("display","none");
+            // } else {
+            //     $("out-thing span").css("display","none")
+            // }
         });
     });
 })
