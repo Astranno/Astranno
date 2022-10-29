704a096be38b0d26e8c8a21799ce3a44d7f9ed09
Christopher Rempe <43194904+MrRicearonie@users.noreply.github.com>
2022-10-29 00:07:46 -0700

    Working on first transition to about

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-$(document).ready(function () {
-    var screenHeight = $(window).height();
-    console.log(screenHeight)
+var screenHeight = $(window).height();
+
+// Here is where the animation on scroll will be kept
+function animation() {
     var controller = new ScrollMagic.Controller();
 
     var tweenStart = new TweenMax.to('#img-logo-start', 4, {
@@ -13,18 +14,37 @@ $(document).ready(function () {
 						.setPin("#starting-page")
                         .addIndicators();
 
+    var tweenAboutTitle = new TweenMax.to('#js-holder-about', 4, {
+        opacity: '100%'
+    })
+
+    var sceneAboutTitle = new ScrollMagic.Scene({duration: (screenHeight/3), offset: (screenHeight/2)})
+                        .setTween(tweenAboutTitle)
+                        .addIndicators();
+
     controller.addScene(sceneStart)
+    controller.addScene(sceneAboutTitle)
+}
+
+$(document).ready(function () {
+    animation()
+
+    $('#js-holder-about').append('<h2 id="js-our-thing" class="text-centered">Making Software is kind of our thing</h2>')
+    $('#our-thing').addClass('hidden')
+    
 
     $(function () {
         $(window).scroll(function () {
             if ($(window).scrollTop() >= (screenHeight/2)) {
                 $("#img-logo-start").css("display","none");
                 $("#name-start").css("display","none");
-                $("#starting-page").css("background-color", "#ffffff");
+                $("body").css("background-color", "#ffffff");
+                $("#js-holder-about").removeClass('hidden')
             } else {
                 $("#img-logo-start").css("display","inline");
                 $("#name-start").css("display","inline");
-                $("#starting-page").css("background-color", "#5bb9e9")
+                $("body").css("background-color", "#5bb9e9")
+                $("#js-holder-about").addClass("hidden")
             }
 
             // if ($(window).scrollTop() >= (screenHight + 1000)) {
