fe818668375b3c3db984a8f2b95faff0a2fca927
Christopher Rempe <43194904+MrRicearonie@users.noreply.github.com>
2022-10-30 01:13:13 -0700

    Testing new layout

--- js/main.js
+++ js/main.js
@@ -15,7 +15,8 @@ function animation() {
                         .addIndicators();
 
     var tweenAboutTitle = new TweenMax.to('#js-holder-about', 4, {
-        opacity: '100%'
+        opacity: '100%',
+        ease: Power2.easeIn
     })
 
     var sceneAboutTitle = new ScrollMagic.Scene({duration: (screenHeight/3), offset: (screenHeight/2)})
@@ -29,22 +30,27 @@ function animation() {
 $(document).ready(function () {
     animation()
 
-    $('#js-holder-about').append('<h2 id="js-our-thing" class="text-centered">Making Software is kind of our thing</h2>')
+    $('#js-holder-about').append('<h2 id="js-our-thing" class="text-centered"><span>Making Software</span> is kind of our thing</h2>')
     $('#our-thing').addClass('hidden')
     
-
+    var position = 0;
     $(function () {
         $(window).scroll(function () {
-            if ($(window).scrollTop() >= (screenHeight/2)) {
+            if (($(window).scrollTop() >= (screenHeight/2)) && ($(window).scrollTop() < (3*screenHeight/2)) && position != 1) {
                 $("#img-logo-start").css("display","none");
                 $("#name-start").css("display","none");
                 $("body").css("background-color", "#ffffff");
-                $("#js-holder-about").removeClass('hidden')
-            } else {
+                $("#js-holder-about").removeClass('hidden').addClass('fullheight-container')
+                position = 1
+            } else if (($(window).scrollTop() < (screenHeight/2)) && position != 0) {
                 $("#img-logo-start").css("display","inline");
                 $("#name-start").css("display","inline");
                 $("body").css("background-color", "#5bb9e9")
                 $("#js-holder-about").addClass("hidden")
+                position = 0
+            } else if (($(window).scrollTop() >= (3*screenHeight/2)) && position != 2) {
+                $("#js-holder-about").addClass("hidden")
+                position = 2
             }
 
             // if ($(window).scrollTop() >= (screenHight + 1000)) {
