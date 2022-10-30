df092902ffb98bd02cec0ce060f87079ffee193d
Christopher Rempe <43194904+MrRicearonie@users.noreply.github.com>
2022-10-30 01:42:32 -0700

    fixing animation length for non macbook

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var screenHeight = $(window).height();
+var animationLength = Math.max(screenHeight, "800")
 
 // Here is where the animation on scroll will be kept
 function animation() {
@@ -9,26 +10,39 @@ function animation() {
         ease: Power2.easeIn
     })
 
-    var sceneStart = new ScrollMagic.Scene({duration: (screenHeight/2), offset: 0})
+    var sceneStart = new ScrollMagic.Scene({duration: (animationLength/2), offset: 0})
                         .setTween(tweenStart)
 						.setPin("#starting-page")
                         .addIndicators();
 
-    var tweenAboutTitle = new TweenMax.to('#js-holder-about', 4, {
+    var tweenAboutTitleIn = new TweenMax.to('#js-holder-about', 4, {
         opacity: '100%',
         ease: Power2.easeIn
     })
 
-    var sceneAboutTitle = new ScrollMagic.Scene({duration: (screenHeight/3), offset: (screenHeight/2)})
-                        .setTween(tweenAboutTitle)
+    var tweenAboutTitleOut = new TweenMax.to('#js-holder-about', 4, {
+        opacity: '0%',
+        ease: Power2.easeOut
+    })
+
+    var sceneAboutTitleIn = new ScrollMagic.Scene({duration: (animationLength/3), offset: (animationLength/2)})
+                        .setTween(tweenAboutTitleIn)
+                        .addIndicators();
+
+    var sceneAboutTitleOut = new ScrollMagic.Scene({duration: (animationLength/3), offset: (5*animationLength/6)})
+                        .setTween(tweenAboutTitleOut)
                         .addIndicators();
 
     controller.addScene(sceneStart)
-    controller.addScene(sceneAboutTitle)
+    controller.addScene(sceneAboutTitleIn)
+    // controller.addScene(sceneAboutTitleOut)
 }
 
 $(document).ready(function () {
     animation()
+
+    console.log(screenHeight)
+    console.log(animationLength)
 
     $('#js-holder-about').append('<h2 id="js-our-thing" class="text-centered"><span>Making Software</span> is kind of our thing</h2>')
     $('#our-thing').addClass('hidden')
@@ -36,22 +50,29 @@ $(document).ready(function () {
     var position = 0;
     $(function () {
         $(window).scroll(function () {
-            if (($(window).scrollTop() >= (screenHeight/2)) && ($(window).scrollTop() < (3*screenHeight/2)) && position != 1) {
-                $("#img-logo-start").css("display","none");
-                $("#name-start").css("display","none");
-                $("body").css("background-color", "#ffffff");
-                $("#js-holder-about").removeClass('hidden').addClass('fullheight-container')
-                position = 1
-            } else if (($(window).scrollTop() < (screenHeight/2)) && position != 0) {
+            if (($(window).scrollTop() >= (3*animationLength/2)) && position < 3) {
+                $("#js-holder-about").addClass("hidden")
+                position = 3
+            } else if (($(window).scrollTop() >= (5*animationLength/6)) && position < 2) {
+                $("#js-holder-about").css("opacity", "100%");
+                console.log("Hello")
+                console.log(position)
+                position = 2
+            } else if (($(window).scrollTop() < (animationLength/2)) && position < 1) {
                 $("#img-logo-start").css("display","inline");
                 $("#name-start").css("display","inline");
                 $("body").css("background-color", "#5bb9e9")
                 $("#js-holder-about").addClass("hidden")
+                position = 1
+            } else if (($(window).scrollTop() >= (animationLength/2)) && ($(window).scrollTop() < (3*animationLength/2)) && position != 0) {
+                $("#img-logo-start").css("display","none");
+                $("#name-start").css("display","none");
+                $("body").css("background-color", "#ffffff");
+                $("#js-holder-about").removeClass('hidden').addClass('fullheight-container')
                 position = 0
-            } else if (($(window).scrollTop() >= (3*screenHeight/2)) && position != 2) {
-                $("#js-holder-about").addClass("hidden")
-                position = 2
             }
+
+
 
             // if ($(window).scrollTop() >= (screenHight + 1000)) {
             //     $("#our-thing span").css("display","none");
